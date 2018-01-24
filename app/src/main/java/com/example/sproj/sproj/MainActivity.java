22ed40a8d12aec782f73d3114d6a14ac3e6c2595
mahnoormmalik <mahnoormmalik@gmail.com>
2018-01-24 21:41:36 +0500

    google json

--- app/src/main/java/com/example/sproj/sproj/MainActivity.java
+++ app/src/main/java/com/example/sproj/sproj/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.sproj.sproj;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        TextView myTextView = (TextView) findViewById(R.id.textView);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/yourfont.ttf");
+        myTextView.setTypeface(typeface);
     }
 }
