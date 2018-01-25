36e34e1834f8a082506a24212b38cbdfa338705d
aimengaba <aimengaba@gmail.com>
2018-01-25 07:57:19 +0500

    commmit

--- app/src/main/java/com/example/sproj/sproj/MainActivity.java
+++ app/src/main/java/com/example/sproj/sproj/MainActivity.java
@@ -1,20 +1,69 @@
 package com.example.sproj.sproj;
 
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.Window;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private StorageReference mStorageRef;
+    ImageView image;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         TextView myTextView = (TextView) findViewById(R.id.textView);
         Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/yourfont.ttf");
+        image = (ImageView)findViewById(R.id.imagedb);
         myTextView.setTypeface(typeface);
+        // Successfully downloaded data to local file
+        // ...
+        // Load the image using Glide
+        Glide.with(this /* context */)
+                .using(new FirebaseImageLoader())
+                .load(mStorageRef)
+                .into(image);
+
+        File localFile = null;
+        try {
+            localFile = File.createTempFile("images", "jpg");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mStorageRef.getFile(localFile)
+                .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle failed download
+                // ...
+            }
+        });
     }
+
 }
