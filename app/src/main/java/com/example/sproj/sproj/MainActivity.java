f427e72752aaf3569e52f5a52da6d0fc2ae8d8f5
aimengaba <aimengaba@gmail.com>
2018-01-25 13:14:18 +0500

    commmit

--- app/src/main/java/com/example/sproj/sproj/MainActivity.java
+++ app/src/main/java/com/example/sproj/sproj/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sproj.sproj;
 
 import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,8 +11,6 @@ import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
-import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FileDownloadTask;
@@ -34,36 +33,37 @@ public class MainActivity extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         TextView myTextView = (TextView) findViewById(R.id.textView);
-        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/yourfont.ttf");
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "Fonts/TitilliumWeb-Black.ttf");
         image = (ImageView)findViewById(R.id.imagedb);
         myTextView.setTypeface(typeface);
         // Successfully downloaded data to local file
         // ...
         // Load the image using Glide
-        Glide.with(this /* context */)
-                .using(new FirebaseImageLoader())
-                .load(mStorageRef)
-                .into(image);
+//        Glide.with(this /* context */)
+//                .using(new FirebaseImageLoader())
+//                .load(mStorageRef)
+//                .into(image);
 
-        File localFile = null;
         try {
-            localFile = File.createTempFile("images", "jpg");
+            final File localFile = File.createTempFile("images", "jpg");
+            mStorageRef.child("bg2.jpg").getFile(localFile)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            image.setImageDrawable(Drawable.createFromPath(localFile.getPath()));
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle failed download
+                    // ...
+                }
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
-        mStorageRef.getFile(localFile)
-                .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
 
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception exception) {
-                // Handle failed download
-                // ...
-            }
-        });
     }
 
 }
